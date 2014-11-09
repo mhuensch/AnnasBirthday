d1ea413ce4dba6fb359952da1b66f50a944f378b
Run00 <setup@run00.com>
2014-11-09 11:18:40 -0600

    Adding FB like

--- index.js
+++ index.js
@@ -37,3 +37,11 @@ setInterval(function () {
 	'<span class="remaining">' + hours + ' Hours ' + minutes +' Minutes ' + seconds + ' to Go!</span>';
 
 }, 1000);
+
+(function(d, s, id) {
+	var js, fjs = d.getElementsByTagName(s)[0];
+	if (d.getElementById(id)) return;
+	js = d.createElement(s); js.id = id;
+	js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0";
+	fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
